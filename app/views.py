338ae8b5d69b586c17a3e9459795383c4c19ad2e
chimolirgb <chimolilucy@gmail.com>
2021-06-06 17:48:18 +0300

    modified files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_source
+from .requests import get_source,article_source,get_category,get_headlines
 # Views
 
 @app.route('/')
@@ -17,10 +17,24 @@ def index():
     return render_template('index.html',title = title, sources=source,headlines = headlines)
 
 
-@app.route('/news/<int:news_id>')
-def news(news_id):
+@app.route('/article/<id>')
+def article(id):
+
 
     '''
     Root function returning index/home page with data
     '''
-    return render_template('news.html',id = news_id)
+  # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
+
+    @app.route('/categories/<cat_name>')
+def category(cat_name):
+  '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+    cat = cat_name
+
+    return render_template('categories.html',title = title,category = category, cat= cat_name)  
