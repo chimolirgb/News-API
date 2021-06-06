0ad29fa21015381a9eabc79a3624fb41aaadc92f
chimolirgb <chimolilucy@gmail.com>
2021-06-06 11:55:27 +0300

    modified:app/views.py,run.py,app/templates/

--- app/views.py
+++ app/views.py
@@ -2,10 +2,22 @@ from flask import render_template
 from app import app
 
 # Views
+
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
+
+    
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    Root function returning index/home page with data
+    '''
+    return render_template('news.html',id = news_id)
