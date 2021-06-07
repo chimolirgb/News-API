9499e0b362a878649f2215c681b707b9e5886410
chimolirgb <chimolilucy@gmail.com>
2021-06-07 15:10:59 +0300

    deleted:app/error.py,modified:app/main/views.py,app/main/error.py

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from flask import render_template,request,redirect,url_for
 from ..request import get_source,article_source,get_category,get_headlines
-from ..models import Review
+# from ..models import Review
 
 #our views
 @main.route('/')
